425c2d3e369b01de74018249fd74f0d683d218dc
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:58:07 +0800

    Add files via upload

---
+++ NET_hw12_ChartPie/NET_hw12_ChartPie/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NET_hw12_ChartPie
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 123);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+			series1.Legend = "Legend1";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(602, 403);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(14, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 25);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Male: ";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.textBox1.Location = new System.Drawing.Point(88, 45);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 34);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.textBox2.Location = new System.Drawing.Point(297, 45);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 34);
+			this.textBox2.TabIndex = 4;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label2.Location = new System.Drawing.Point(201, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 25);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Female: ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.Location = new System.Drawing.Point(433, 27);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(98, 25);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Male(%) :";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label4.Location = new System.Drawing.Point(411, 79);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(120, 25);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Female(%) :";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(632, 538);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.chart1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ NET_hw12_ChartPie/NET_hw12_ChartPie/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace NET_hw12_ChartPie
+{
+	public partial class Form1 : Form
+	{
+		private double[] person;// series Y
+		private string[] name;//series X
+		private Series series;
+		public Form1()
+		{
+			InitializeComponent();
+			series = new Series();
+			person = new double[2];
+			person[0] = 0;
+			person[1] = 0;
+			name = new string[] { "Male", "Female" };
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			count(textBox1.Text, textBox2.Text);
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			count(textBox1.Text, textBox2.Text);
+		}
+
+		private void count(string male,string female)
+		{
+			chart1.Series.Clear();
+			//count male
+			try
+			{
+				this.person[0] = Convert.ToDouble(male);
+			}
+			catch (System.NullReferenceException)
+			{
+				this.person[0] = 0;
+			}
+			catch (System.FormatException)
+			{
+				this.person[0] = 0;
+			}
+
+			//count female
+			try
+			{
+				this.person[1] = Convert.ToDouble(female);
+			}
+			catch (System.NullReferenceException)
+			{
+				this.person[1] = 0;
+			}
+			catch (System.FormatException)
+			{
+				this.person[1] = 0;
+			}
+			series.ChartType = SeriesChartType.Pie;
+			series.Points.DataBindXY( this.name , this.person);
+			//series.Points.DataBindY(this.person);-->show pie only, no data binding
+			chart1.Series.Add(series);
+			chart1.Series[0].Points[0]["PieLabelStyle"] = "Disabled";
+			chart1.Series[0].Points[1]["PieLabelStyle"] = "Disabled";
+
+			double sum = this.person[0] + this.person[1];
+			double percent_Male = this.person[0] / sum*100;
+			double percent_Female = this.person[1] / sum*100;
+			label3.Text = "Male(%) :" + percent_Male.ToString("0.0") +" %";
+			label4.Text = "Female(%) :" + percent_Female.ToString("0.0") +" %";
+		}
+	}
+}
